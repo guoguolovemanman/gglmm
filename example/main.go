fce28ba8d6b9b8af0d8d33f240a4af7b4429f4d1
weihongguo <35117668@qq.com>
2020-05-08 22:37:29 +0800

    example

--- example/main.go
+++ example/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/rpc"
@@ -12,46 +14,51 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-// Test --
-type Test struct {
+// Example --
+type Example struct {
 	gglmm.Model
 	IntValue    int     `json:"intValue"`
 	FloatValue  float64 `json:"floatValue"`
 	StringValue string  `json:"stringValue"`
 }
 
+// ResponseKey --
+func (example Example) ResponseKey() [2]string {
+	return [...]string{"example", "examples"}
+}
+
 // Cache --
-func (test Test) Cache() bool {
+func (example Example) Cache() bool {
 	return true
 }
 
-// TestFunc --
-func TestFunc(w http.ResponseWriter, r *http.Request) {
-
+// ExampleAction --
+func ExampleAction(w http.ResponseWriter, r *http.Request) {
+	gglmm.OkResponse().JSON(w)
 }
 
-// RPCTestService --
-type RPCTestService struct {
+// RPCExampleService --
+type RPCExampleService struct {
 	repository *gglmm.GormRepository
 }
 
-// NewRPCTestService --
-func NewRPCTestService() *RPCTestService {
-	return &RPCTestService{
+// NewRPCExampleService --
+func NewRPCExampleService() *RPCExampleService {
+	return &RPCExampleService{
 		repository: gglmm.DefaultGormRepository(),
 	}
 }
 
 // Actions --
-func (service *RPCTestService) Actions(cmd string, actions *[]*gglmm.RPCAction) error {
+func (service *RPCExampleService) Actions(cmd string, actions *[]*gglmm.RPCAction) error {
 	*actions = append(*actions, gglmm.NewRPCAction("Get", "string", "*Test"))
 	*actions = append(*actions, gglmm.NewRPCAction("List", "gglmm.FilterRequest", "*[]Test"))
 	return nil
 }
 
 // Get --
-func (service *RPCTestService) Get(idRequest gglmm.IDRequest, test *Test) error {
-	err := service.repository.Get(test, idRequest)
+func (service *RPCExampleService) Get(idRequest gglmm.IDRequest, example *Example) error {
+	err := service.repository.Get(example, idRequest)
 	if err != nil {
 		return err
 	}
@@ -59,8 +66,8 @@ func (service *RPCTestService) Get(idRequest gglmm.IDRequest, test *Test) error 
 }
 
 // List --
-func (service *RPCTestService) List(filterRequest gglmm.FilterRequest, tests *[]Test) error {
-	service.repository.List(tests, filterRequest)
+func (service *RPCExampleService) List(filterRequest gglmm.FilterRequest, examples *[]Example) error {
+	service.repository.List(examples, filterRequest)
 	return nil
 }
 
@@ -72,25 +79,26 @@ func main() {
 	defer redisCacher.Close()
 	gglmm.RegisterCacher(redisCacher)
 
-	gglmm.BasePath("/api/example")
+	gglmm.BasePath("/api")
 
-	testHTTPService := gglmm.NewHTTPService(Test{}).
+	exampleService := gglmm.NewHTTPService(Example{}).
 		HandleBeforeCreateFunc(beforeCreate).
 		HandleBeforeUpdateFunc(beforeUpdate)
-	gglmm.HandleHTTP("/test", testHTTPService).
+	gglmm.HandleHTTP("/example", exampleService).
 		Action(gglmm.Middleware{
-			Name: "example",
+			Name: "ExampleMiddleware",
 			Func: middlewareFunc,
 		}, gglmm.ReadActions, gglmm.WriteActions, gglmm.DeleteActions)
 
-	gglmm.HandleHTTPAction("/test_func", TestFunc, "GET", "POST").
+	gglmm.HandleHTTPAction("/example_action", ExampleAction, "GET", "POST").
 		Middleware(gglmm.Middleware{
-			Name: "test_func",
+			Name: "ExampleMiddleware",
 			Func: middlewareFunc,
 		})
 
-	gglmm.RegisterRPC(NewRPCTestService())
+	gglmm.RegisterRPC(NewRPCExampleService())
 
+	go testHTTP()
 	go testRPC()
 
 	gglmm.ListenAndServe(":10000")
@@ -107,48 +115,75 @@ func middlewareFunc(next http.Handler) http.Handler {
 
 func beforeCreate(model interface{}) (interface{}, error) {
 	log.Printf("%#v\n", model)
-	test, ok := model.(*Test)
+	example, ok := model.(*Example)
 	if !ok {
 		return nil, gglmm.ErrModelType
 	}
-	test.StringValue = "string"
-	return test, nil
+	example.StringValue = "string"
+	return example, nil
 }
 
 func beforeUpdate(model interface{}, id int64) (interface{}, int64, error) {
-	test, ok := model.(*Test)
+	example, ok := model.(*Example)
 	if !ok {
 		return nil, 0, gglmm.ErrModelType
 	}
-	test.StringValue = "string"
-	return test, id, nil
+	example.StringValue = "string"
+	return example, id, nil
+}
+
+func testHTTP() {
+	time.Sleep(4 * time.Second)
+
+	response, err := http.Get("http://localhost:10000/api/example/1")
+	if err != nil {
+		log.Println("http", err)
+		return
+	}
+	defer response.Body.Close()
+
+	result, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Println("ReadAll", err)
+		return
+	}
+
+	fmt.Println()
+	log.Println(string(result))
 }
 
 func testRPC() {
 	time.Sleep(2 * time.Second)
+
 	client, err := rpc.DialHTTP("tcp", ":10000")
 	if err != nil {
 		log.Println("rpc", err)
 		return
 	}
 
+	fmt.Println()
+
 	idRequest := gglmm.IDRequest{
 		ID: 1,
 	}
-	test := Test{}
-	err = client.Call("RPCTestService.Get", idRequest, &test)
+	example := Example{}
+	err = client.Call("RPCExampleService.Get", idRequest, &example)
 	if err != nil {
-		log.Println("RPCTestService.Get", err)
+		log.Println("RPCExampleService.Get", err)
 	} else {
-		log.Printf("Get: %#v", test)
+		log.Printf("Get: \n%+v", example)
 	}
 
+	fmt.Println()
+
 	filterRequest := gglmm.FilterRequest{}
-	tests := make([]Test, 0)
-	err = client.Call("RPCTestService.List", filterRequest, &tests)
+	filterRequest.AddFilter("id", gglmm.FilterOperateGreaterEqual, 2)
+	filterRequest.AddFilter("id", gglmm.FilterOperateLessThan, 4)
+	examples := make([]Example, 0)
+	err = client.Call("RPCExampleService.List", filterRequest, &examples)
 	if err != nil {
-		log.Println("RPCTestService.List", err)
+		log.Println("RPCExampleService.List", err)
 	} else {
-		log.Printf("List: %#v", tests)
+		log.Printf("List: \n%+v", examples)
 	}
 }
--- example/migration.sql
+++ example/migration.sql
@@ -1,13 +1,13 @@
 
-drop table if exists `test`;
+drop table if exists example;
 
-create table if not exists `test` (
-  `id` bigint unsigned not null auto_increment,
-  `created_at` timestamp null default null,
-  `updated_at` timestamp null default null,
-  `deleted_at` timestamp null default null,
-  `int_value` int not null,
-  `float_value` float(10, 2) not null,
-  `string_value` varchar(255) not null,
-  primary key (`id`)
+create table if not exists example (
+  id bigint unsigned not null auto_increment,
+  created_at timestamp null default null,
+  updated_at timestamp null default null,
+  deleted_at timestamp null default null,
+  int_value int not null,
+  float_value float(10, 2) not null,
+  string_value varchar(255) not null,
+  primary key (id)
 );
